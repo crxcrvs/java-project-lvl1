49aa2fc4fa075ae0f9aebc3252374b95701ec9f2
Alexander D <derevyanko.alexander@yahoo.com>
2021-11-06 13:09:03 +0200

    Project is ready for building and generating executable file

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,6 +2,6 @@ package hexlet.code;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.prinln("Hello World!");
+		System.out.println("Welcome to the Brain Games!");
 	}
 }
